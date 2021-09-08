b4fad1cc07ebbc2b4b7c16d317577ffb114c4502
ronnydennis <64128559+ronnyden@users.noreply.github.com>
2021-09-08 03:38:53 -0400

    Add files via upload

---
+++ app/Http/Controllers/Comments.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comment;
+
+class Comments extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $comment = new Comment;
+        $comment->name  = $data['name'];//should be loaded dynamically from user account
+        $comment->email = $data['email'];//should be loaded dynamically from user account
+        $comment->comment = $data['comment'];
+        $comment->save();
+        
+        return response("comment uploaded",201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $comment = Comment::find($id);
+        return response()->json($comment);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $comment = Comment::find($id);
+        $comment->comment = $request['comment'];
+        $comment->save();
+        return response("Updated",200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+       $comment = Comment::find($id)->delete();
+        return response("Comment deleted",404);
+    }
+}
---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Sanctum\HasApiTokens;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsersController extends Controller
+{
+    public function createUser(Request $request){
+       $request->validate([
+         'name'=>'required|string',
+         'email'=>'required|string',
+         'password'=>'required|string|min:8'
+       ]);
+      $password = Hash::make($request['password']);
+       User::create([
+          'name' => $request['name'],
+          'email'=> $request['email'],
+          'password'=>Hash::make($request['password'])
+       ]
+           
+       );
+       return response("User added",201);
+    }
+    public function login(Request $request){
+        $user = User::where( 'email',$request['email'])->get()->first();
+
+        if(!$user || !Hash::check($request['password'],$user->password)){
+            return response("Wrong cridentials",401);
+        }
+         $token = $user->createToken('personal_access_tokens')->plainTextToken;
+
+         $response  = [
+             'user' => $user,
+             'token' => $token
+         ];
+         return response($response);
+        
+    }
+
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+
+        return response("Logged out",403);
+        
+    }
+}
